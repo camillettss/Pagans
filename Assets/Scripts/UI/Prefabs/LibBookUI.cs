c6e80d91e0f75d2d3c6f64413db7e442ca8702b3
Francesco Camilletti <hijok3r@gmail.com>
2021-11-23 00:44:52 +0100

    libUI ended

--- Assets/Scripts/UI/Prefabs/LibBookUI.cs
+++ Assets/Scripts/UI/Prefabs/LibBookUI.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Animator))]
 public class LibBookUI : MonoBehaviour
 {
     public Image icon;
     public Text nameText;
+    public string story;
 
-    public void SetData(Book book)
+    public void SetData(StoryBook book)
     {
         icon.sprite = book.icon;
         nameText.text = book.Name;
+        story = book.story;
+    }
+
+    public void choose()
+    {
+        GetComponent<Animator>().SetTrigger("choose");
     }
 }
