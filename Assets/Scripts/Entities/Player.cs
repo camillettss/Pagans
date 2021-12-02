d6f55174a6983d57f5c6eb77fe736c8b098c4c84
camillettss <34480071+Alfacecc3@users.noreply.github.com>
2021-12-02 14:41:26 +0100

    tileset update

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -125,7 +125,7 @@ public class Player : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.P))
         {
-            
+            //growObjectsTilemap.GetTile(new Vector3Int((int)transform.position.x, (int)transform.position.y, 0));
         }
 
         // minimap show
