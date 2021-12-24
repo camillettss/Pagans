00876b3b87e6ef3c3fb742efc4c880c35aba0537
Francesco Camilletti <hijok3r@gmail.com>
2021-12-24 19:05:15 +0100

    NoAI

---
+++ Assets/Scripts/AI/NoAIBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoAIBehaviour : MonoBehaviour
+{
+    public void AtHour(int hour)
+    {
+
+    }
+}
